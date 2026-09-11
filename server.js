cd5c8d1d512f2600102e8dca5433f3155f881a59
Casa Sabor <tu-correo@example.com>
2026-09-11 15:57:50 -0500

    Fix fs import for image folder

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcrypt");
 const session = require("express-session");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
 
